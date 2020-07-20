2c62ad67c5bd32fa0cdaa291af0bc5a667d62c17
Toan211 <51894089+Toan211@users.noreply.github.com>
2020-07-20 10:09:35 +0700

    Huffman Coding Text

---
+++ huffman_Text/huffman/Source.cpp
@@ -0,0 +1,210 @@
+// C++ program to encode and decode a string using 
+// Huffman Coding. 
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+#include <unordered_map>
+#include <queue>
+
+#include <chrono> 
+
+#define MAX_TREE_HT 256 
+using namespace std;
+using namespace std::chrono;
+
+// to map each character its huffman value 
+map<char, string> codes;
+
+// to store the frequency of character of the input data 
+map<char, int> freq;
+string stream_as_string(istream& stm)
+{
+    return { istreambuf_iterator<char>(stm), std::istreambuf_iterator<char>{} };
+}
+// A Huffman tree node 
+struct MinHeapNode
+{
+    char data;             // One of the input characters 
+    int freq;             // Frequency of the character 
+    MinHeapNode* left, * right; // Left and right child 
+
+    MinHeapNode(char data, int freq)
+    {
+        left = right = NULL;
+        this->data = data;
+        this->freq = freq;
+    }
+};
+
+// utility function for the priority queue 
+struct compare
+{
+    bool operator()(MinHeapNode* l, MinHeapNode* r)
+    {
+        return (l->freq > r->freq);
+    }
+};
+
+// utility function to print characters along with 
+// there huffman value 
+void printCodes(struct MinHeapNode* root, string str)
+{
+    if (!root)
+        return;
+    if (root->data != '$')
+        std::cout << root->data << ": " << str << "\n";
+    printCodes(root->left, str + "0");
+    printCodes(root->right, str + "1");
+}
+
+// utility function to store characters along with 
+// there huffman value in a hash table, here we 
+// have C++ STL map 
+void storeCodes(struct MinHeapNode* root, string str)
+{
+    if (root == NULL)
+        return;
+    if (root->data != '$')
+        codes[root->data] = str;
+    storeCodes(root->left, str + "0");
+    storeCodes(root->right, str + "1");
+}
+
+// STL priority queue to store heap tree, with respect 
+// to their heap root node value 
+priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+
+// function to build the Huffman tree and store it 
+// in minHeap 
+void HuffmanCodes(int size)
+{
+    struct MinHeapNode* left, * right, * top;
+    for (map<char, int>::iterator v = freq.begin(); v != freq.end(); v++)
+        minHeap.push(new MinHeapNode(v->first, v->second));
+    while (minHeap.size() != 1)
+    {
+        left = minHeap.top();
+        minHeap.pop();
+        right = minHeap.top();
+        minHeap.pop();
+        top = new MinHeapNode('$', left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        minHeap.push(top);
+    }
+    storeCodes(minHeap.top(), "");
+}
+
+// utility function to store map each character with its 
+// frequency in input string 
+void calcFreq(string str, int n)
+{
+    for (int i = 0; i < str.size(); i++)
+        freq[str[i]]++;
+}
+
+// function iterates through the encoded string s 
+// if s[i]=='1' then move to node->right 
+// if s[i]=='0' then move to node->left 
+// if leaf node append the node->data to our output string 
+string decode_file(struct MinHeapNode* root, string s)
+{
+    string ans = "";
+    struct MinHeapNode* curr = root;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == '0')
+            curr = curr->left;
+        else
+            curr = curr->right;
+
+        // reached leaf node 
+        if (curr->left == NULL and curr->right == NULL)
+        {
+            ans += curr->data;
+            curr = root;
+        }
+    }
+    // cout<<ans<<endl; 
+    return ans + '\0';
+}
+
+// Driver program to test above functions 
+int main()
+{
+#if 1
+    ofstream OutFile;
+    OutFile.open("OutPut2-10k.txt"/*, ios::binary */); //output file
+    if (!OutFile) {
+        std::cout << "Cannot open file!" << endl;
+        return 1;
+    }
+    string encodedString, decodedString;
+    ifstream inFile(/*copy file text path: Example*/"C:/Users/Admin/source/repos/huffman_Text/huffman/test/OutPut2-10k.txt");
+    string str;
+    str = stream_as_string(inFile);
+
+
+    // Get starting timepoint 
+    auto start = high_resolution_clock::now();
+
+    calcFreq(str, str.length());
+    HuffmanCodes(str.length());
+
+    // Get ending timepoint 
+    auto stop = high_resolution_clock::now();
+
+    std::cout << "Character With there Frequencies:\n";
+    for (auto v = codes.begin(); v != codes.end(); v++)
+        std::cout << v->first << ' ' << v->second << endl;
+
+    for (auto i : str)
+        encodedString += codes[i];
+
+   
+
+    std::cout <<"Compressed Number of Bytes: "<< (encodedString.length())/8 << " bytes"<<endl;
+     OutFile << encodedString << endl;
+
+     
+     // Get duration. Substart timepoints to  
+    // get durarion. To cast it to proper unit 
+    // use duration cast method 
+     auto duration = duration_cast<microseconds>(stop - start);
+     std::cout << "Time taken by function: "<< duration.count() << " microseconds" << endl;
+    /*
+    
+    decodedString = decode_file(minHeap.top(), encodedString);
+    OutPut << decodedString << endl; */
+    return 0;
+#endif
+
+#pragma region Error Decode
+#if 0
+    string encodedString, decodedString;
+    ifstream inFile("C:/Users/Admin/source/repos/huffman/huffman/OutPut2-10k.txt");
+    string str;
+    str = stream_as_string(inFile);
+    calcFreq(str, str.length());
+    HuffmanCodes(str.length());
+    cout << "Character With there Frequencies:\n";
+    for (auto v = codes.begin(); v != codes.end(); v++)
+        cout << v->first << ' ' << v->second << endl;
+
+    for (auto i : str)
+        encodedString += codes[i];
+
+    //OutFile << encodedString << endl;
+
+    /*
+    ifstream inFile("C:/Users/Admin/source/repos/huffman/huffman/OutPut2-10k.txt");
+    string str;
+    */
+    decodedString = decode_file(minHeap.top(), encodedString);
+   // OutFile << decodedString << endl;
+    return 0; 
+#endif
+#pragma endregion
+}
+
